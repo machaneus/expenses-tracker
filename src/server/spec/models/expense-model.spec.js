2ddf1c68b1f6f66ecb30c88496a13896309fae46
machaneus <machaneus@gmail.com>
2017-09-28 22:29:19 +0300

    Implement filtering functionality and update necessity, category tests accordingly

--- src/server/spec/models/expense-model.spec.js
+++ src/server/spec/models/expense-model.spec.js
@@ -24,8 +24,8 @@ describe('Expense model tests:', () => {
   describe('Expense model find', () => {
     it('Should perform a SELECT query with no WHERE if no filter is specified', () => {
 
-      client.query = (queryStr, callback) => {
-        clientSpy(queryStr);
+      client.query = (queryStr, values, callback) => {
+        clientSpy(queryStr, values);
         callback(null, {});
       };
 
@@ -38,14 +38,52 @@ describe('Expense model tests:', () => {
       expenseModel.find({}, findCallback);
 
       clientSpy.calledWith(queryStr).should.equal(true, 'Bad queryStr ' + clientSpy.args[0][0]);
+      clientSpy.args[0][1].length.should.equal(0);
       done.calledOnce.should.equal(true, 'Done not called at all or called more than once ');
     });
-    it('Should perform a SELECT ... WHERE category=... if category filter is specified');
-    it('Should perform a SELECT ... WHERE necessity=... if necessity filter is specified');
+    it('Should perform a SELECT ... WHERE category=... if category filter is specified', () => {
+      client.query = (queryStr, values, callback) => {
+        clientSpy(queryStr, values);
+        callback(null, {});
+      };
+
+      let queryStr = 'SELECT value, necessity, category, timestamp FROM expense WHERE category=$1';
+      pool.connect = (callback) => {
+        callback(null, client, done);
+      };
+
+      expenseModel = require('../../src/models/expense-model')(pool);
+      expenseModel.find({ category: { value: 'food', operator: '=' } }, findCallback);
+
+      clientSpy.calledWith(queryStr).should.equal(true, 'Bad queryStr: ' + clientSpy.args[0][0]);
+      clientSpy.args[0][1].length.should.equal(1);
+      clientSpy.args[0][1][0].should.equal('food');
+      done.calledOnce.should.equal(true, 'Done not called at all or called more than once ');
+    });
+    it('Should perform a SELECT ... WHERE necessity=... if necessity filter is specified', () => {
+      client.query = (queryStr, values, callback) => {
+        clientSpy(queryStr, values);
+        callback(null, {});
+      };
+
+      let queryStr = 'SELECT value, necessity, category, timestamp FROM expense WHERE necessity=$1';
+      pool.connect = (callback) => {
+        callback(null, client, done);
+      };
+
+      expenseModel = require('../../src/models/expense-model')(pool);
+      expenseModel.find({ necessity: { value: 'low', operator: '=' } }, findCallback);
+
+      clientSpy.calledWith(queryStr).should.equal(true, 'Bad queryStr: ' + clientSpy.args[0][0]);
+      clientSpy.args[0][1].length.should.equal(1);
+      clientSpy.args[0][1][0].should.equal('low');
+      done.calledOnce.should.equal(true, 'Done not called at all or called more than once ');
+    });
     it('Should perform a SELECT ... WHERE value^(=|>|<)$... if value filter and operator is specified');
     it('Should return results on succesfull query');
     it('Should call callback with connection error if pool connection fails');
     it('Should call callback with query error if query fails');
+    it('Should sanitize queries');
   });
 
   describe('Expense model save', () => {
--- src/server/src/models/expense-model.js
+++ src/server/src/models/expense-model.js
@@ -5,19 +5,26 @@
 
 let expensesModel = (pool) => {
   let find = (filter, callback) => {
-    let queryStr;
+    let queryStr = 'SELECT value, necessity, category, timestamp FROM expense';
     pool.connect((err, client, done) => {
       if (err) {
         callback(new Error('Error on db connection: ' + err), undefined)
       }
 
-      if (!filter.category && !filter.value && !filter.necessity) {
-        queryStr = 'SELECT value, necessity, category, timestamp FROM expense';
+      let whereFilters = [];
+      let whereValues = [];
+      Object.keys(filter).forEach(function (key, index) {
+        whereFilters.push(key + filter[key].operator + '$' + (index + 1));
+        whereValues.push(filter[key].value);
+      });
+
+      if (whereFilters.length > 0) {
+        queryStr += ' WHERE ' + whereFilters.join(', ');
       }
-      client.query(queryStr, (error, results) => {
+      console.log(queryStr);
+      client.query(queryStr, whereValues, (error, results) => {
         done();
         if (error) {
-          console.log(error);
           callback(new Error('Error on query: ' + error), undefined);
         } else {
           callback(null, results);
